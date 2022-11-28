853210cf99c100dc76b04f11ae255bf213a038f9
HyunisEmpty <105339034+HyunisEmpty@users.noreply.github.com>
2022-11-29 02:34:34 +0900

    Add files via upload
    기초 코드 구현

---
+++ main.py
@@ -0,0 +1,22 @@
+from ui import Game
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+    QHBoxLayout, QVBoxLayout, QApplication, QLabel,
+    QComboBox, QTextEdit, QLineEdit)
+
+
+def gameMain():
+    food = 10
+    day = 1
+    app = QApplication(sys.argv)
+    game = Game()
+    game.showGame(food, day, "first_start")
+
+
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    gameMain()
